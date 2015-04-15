c23d193c4af8bad6ec2a1efe7ad8263d62c36d86
kpdecker <kpdecker@gmail.com>
2015-04-15 12:20:18 -0500

    Fix tests under Circus 4.1.1

--- test/plugins/loader.js
+++ test/plugins/loader.js
@@ -92,15 +92,23 @@ describe('loader plugin', function() {
         "files": [
           "bundle.js",
           "0.bundle.css",
+          "bundle.js.map",
           "1.bundle.js",
+          "1.bundle.js.map",
           "2.bundle.js",
+          "2.bundle.js.map",
           "3.bundle.js",
+          "3.bundle.js.map"
         ],
         "published": {
           "bundle.js": "bundle.js",
+          "bundle.js.map": "bundle.js.map",
           "1.bundle.js": "1.bundle.js",
+          "1.bundle.js.map": "1.bundle.js.map",
           "2.bundle.js": "2.bundle.js",
+          "2.bundle.js.map": "2.bundle.js.map",
           "3.bundle.js": "3.bundle.js",
+          "3.bundle.js.map": "3.bundle.js.map",
           "0.bundle.css": "0.bundle.css"
         },
         "entry": "bundle.js",
@@ -173,13 +181,19 @@ describe('loader plugin', function() {
         },
         "files": [
           "bundle.js",
+          "bundle.js.map",
           "1.bundle.js",
+          "1.bundle.js.map",
           "2.bundle.js",
+          "2.bundle.js.map"
         ],
         "published": {
           "bundle.js": "bundle.js",
+          "bundle.js.map": "bundle.js.map",
           "1.bundle.js": "1.bundle.js",
+          "1.bundle.js.map": "1.bundle.js.map",
           "2.bundle.js": "2.bundle.js",
+          "2.bundle.js.map": "2.bundle.js.map"
         },
         "entry": "bundle.js",
         "usedModules": []
