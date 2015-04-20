75390ffa92a06daeb6a97ae9e86f1696539fec17
kpdecker <kpdecker@gmail.com>
2015-04-20 18:00:14 -0500

    Fix tests under latest webpack

--- test/plugins/loader.js
+++ test/plugins/loader.js
@@ -93,6 +93,7 @@ describe('loader plugin', function() {
           'bundle.js',
           '0.bundle.css',
           'bundle.js.map',
+          '0.bundle.css.map',
           '1.bundle.js',
           '1.bundle.js.map',
           '2.bundle.js',
@@ -109,7 +110,8 @@ describe('loader plugin', function() {
           '2.bundle.js.map': '2.bundle.js.map',
           '3.bundle.js': '3.bundle.js',
           '3.bundle.js.map': '3.bundle.js.map',
-          '0.bundle.css': '0.bundle.css'
+          '0.bundle.css': '0.bundle.css',
+          '0.bundle.css.map': '0.bundle.css.map'
         },
         'entry': 'bundle.js',
         'usedModules': []
